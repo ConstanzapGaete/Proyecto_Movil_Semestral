4aaccd8d3d647b219efa9110d54e899819a3da7f
Temism <marcopalmasoto@gmail.com>
2024-10-25 20:03:01 -0300

    geolocalizacion aplicada

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,7 +3,12 @@ import { NavController, MenuController } from '@ionic/angular';
 import { FirebaseService } from 'src/app/Services/firebase.service';
 import { Subscription } from 'rxjs';
 import { AlertController } from '@ionic/angular';
-import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner'; 
+import {
+  CapacitorBarcodeScanner,
+  CapacitorBarcodeScannerTypeHint,
+} from '@capacitor/barcode-scanner';
+import { Geolocation } from '@capacitor/geolocation';
+import { GeocodingService } from 'src/app/Services/geolocalizacion.service';
 
 @Component({
   selector: 'app-home',
@@ -15,18 +20,29 @@ export class HomePage implements OnInit, OnDestroy {
   private authSubscription: Subscription;
   isSupported = false;
   result: string = '';
+  latitud: number | null = null;
+  longitud: number | null = null;
+  dia: string = '';
+  fechaa: string = '';
+  horas: string = '';
+  ubicacion: string = '';
 
   constructor(
     private navCtrl: NavController,
     private firebaseService: FirebaseService,
     private menuCtrl: MenuController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private ubi: GeocodingService
   ) {}
 
-  ngOnInit() {
-    this.authSubscription = this.firebaseService.getAuthState().subscribe((user) => {
-      this.nombreUsuario = user ? this.extraerNombre(user.email) : 'Invitado';
-    });
+  async ngOnInit() {
+    this.authSubscription = this.firebaseService
+      .getAuthState()
+      .subscribe((user) => {
+        this.nombreUsuario = user ? this.extraerNombre(user.email) : 'Invitado';
+      });
+    await this.ObtenerUbicacion();
+    this.obtenerFecha();
   }
 
   ngOnDestroy() {
@@ -60,12 +76,11 @@ export class HomePage implements OnInit, OnDestroy {
   async scan(): Promise<void> {
     try {
       const result = await CapacitorBarcodeScanner.scanBarcode({
-        hint: CapacitorBarcodeScannerTypeHint.ALL, 
+        hint: CapacitorBarcodeScannerTypeHint.ALL,
       });
 
-
       if (result && result.ScanResult) {
-        this.result = result.ScanResult; 
+        this.result = result.ScanResult;
         this.presentAlert('Código escaneado', result.ScanResult);
         console.log('Código escaneado:', this.result);
       } else {
@@ -90,7 +105,39 @@ export class HomePage implements OnInit, OnDestroy {
     this.menuCtrl.enable(true);
   }
   extraerNombre(email: string): string {
-    const nombre = email.split('@')[0]; 
+    const nombre = email.split('@')[0];
     return nombre.charAt(0).toUpperCase() + nombre.slice(1);
+  }
+
+  async ObtenerUbicacion() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.latitud = coordinates.coords.latitude;
+      this.longitud = coordinates.coords.longitude;
+      console.log('Latitud:', this.latitud, 'Longitud:', this.longitud);
+      this.ubicacion = await this.ubi.getLocation(this.latitud, this.longitud);
+    } catch (error) {
+      console.error('Error al obtener ubicación:', error);
+    }
+  }
+
+  obtenerFecha() {
+    const fecha = new Date();
+
+    const opcionesFecha: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    const opcionesDia: Intl.DateTimeFormatOptions = { weekday: 'long' };
+    const opcionesHora: Intl.DateTimeFormatOptions = {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+
+    this.fechaa = fecha.toLocaleDateString('es-ES', opcionesFecha);
+    this.dia = fecha.toLocaleDateString('es-ES', opcionesDia);
+    this.horas = fecha.toLocaleTimeString('es-ES', opcionesHora);
   }
 }

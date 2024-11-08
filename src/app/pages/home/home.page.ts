f2322320a81cec115cf44aafd308c0a43c78e57c
Temism <marcopalmasoto@gmail.com>
2024-11-08 02:27:09 -0300

    cambios

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,12 +3,9 @@ import { NavController, MenuController } from '@ionic/angular';
 import { FirebaseService } from 'src/app/Services/firebase.service';
 import { Subscription } from 'rxjs';
 import { AlertController } from '@ionic/angular';
-import {
-  CapacitorBarcodeScanner,
-  CapacitorBarcodeScannerTypeHint,
-} from '@capacitor/barcode-scanner';
 import { Geolocation } from '@capacitor/geolocation';
 import { GeocodingService } from 'src/app/Services/geolocalizacion.service';
+import { BrowserMultiFormatReader } from '@zxing/browser';
 
 @Component({
   selector: 'app-home',
@@ -73,25 +70,6 @@ export class HomePage implements OnInit, OnDestroy {
     window.open(url, '_blank');
   }
 
-  async scan(): Promise<void> {
-    try {
-      const result = await CapacitorBarcodeScanner.scanBarcode({
-        hint: CapacitorBarcodeScannerTypeHint.ALL,
-      });
-
-      if (result && result.ScanResult) {
-        this.result = result.ScanResult;
-        this.presentAlert('Código escaneado', result.ScanResult);
-        console.log('Código escaneado:', this.result);
-      } else {
-        this.presentAlert('Error', 'No se pudo escanear el código.');
-      }
-    } catch (error) {
-      console.error('Error al escanear el código:', error);
-      this.presentAlert('Error', 'Ocurrió un error al intentar escanear.');
-    }
-  }
-
   async presentAlert(header: string, message: string): Promise<void> {
     const alert = await this.alertController.create({
       header,
@@ -142,6 +120,6 @@ export class HomePage implements OnInit, OnDestroy {
   }
 
   justificarAsistencia() {
-    this.navCtrl.navigateForward('/justificara'); 
+    this.navCtrl.navigateForward('/justificara');
   }
 }

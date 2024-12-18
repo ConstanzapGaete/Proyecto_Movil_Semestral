48be4b3e2b0d37773445ce76576bb95aa2b21019
Temism <marcopalmasoto@gmail.com>
2024-12-17 21:50:58 -0300

    asdads

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,17 +6,6 @@ import { database } from 'firebase-admin';
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyDGmzchrQCLqcGKScd2JXOmY67aJZcQGPQ',
-    authDomain: 'aplicaciones-moviles-91302.firebaseapp.com',
-    projectId: 'aplicaciones-moviles-91302',
-    storageBucket: 'aplicaciones-moviles-91302.appspot.com',
-    messagingSenderId: '1058072318647',
-    appId: '1:1058072318647:web:ba64d8ebcd177581077332',
-    measurementId: 'G-K78779NPG0',
-    databaseURL:
-      'https://aplicaciones-moviles-91302-default-rtdb.firebaseio.com',
-  },
 };
 
 /*
